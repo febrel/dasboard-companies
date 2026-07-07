f24770290dd62f02bf0e5c4fa381724213c6e805
febrel <febreluis06@gmail.com>
2026-07-07 09:01:41 -0500

    fix: build

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,10 +1,10 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Sidebar from "@/components/Sidebar/Sidebar";
 
-export default function LayoutDasboard({
+export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactElement;
+  children: React.ReactNode;
 }) {
   return (
     <div className="flex w-full h-full">
